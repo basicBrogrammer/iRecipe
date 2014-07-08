d5ca8220f01b84f8c6fb9b98d1f207a43900e74e
Jeremy <jcward10@gmail.com>
2014-07-08 14:04:15 -0400

    working on mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
         :address   => "smtp.mandrillapp.com",
         :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
         :enable_starttls_auto => true, # detects and uses STARTTLS
-        :user_name =>      ENV['MANDRILL_USERNAME'],
-        :password =>       ENV['MANDRILL_APIKEY'],
+        # :user_name =>      ENV['MANDRILL_USERNAME'],
+        # :password =>       ENV['MANDRILL_APIKEY'],
         :domain =>         'heroku.com',
         :authentication => :plain
     }
